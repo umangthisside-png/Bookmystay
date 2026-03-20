acb538d73f940634c1de432c4386a1d7d8b94d49
sid01-dev <sid01.dev@gmail.com>
2026-03-20 14:31:20 +0530

    Initial commit - Book My Stay App

---
+++ BOOKMYSTAY/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,24 @@
+/**
+ * -----------------------------------------------------------
+ * MAIN CLASS - UseCase1HotelBookingApp
+ * -----------------------------------------------------------
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * Entry point of the Hotel Booking Management System.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("Application : Book My Stay App");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
